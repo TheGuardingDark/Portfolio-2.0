2ad121e5ff7e5d2dadb136b9540eabfec60abd78
Scout <therearespidersinthisemail@gmail.com>
2019-08-17 23:34:39 -0600

    added slide down top nav bar and banner video

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+function openSideNav() {
+    document.getElementById("slideNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("slideNav").style.height = "0%";
+}

8269f5be54ee95a0cdda054e07b76e637b79f5b9
Scout <therearespidersinthisemail@gmail.com>
2019-10-13 18:53:24 -0600

    changed way portfolio pieces are displayed to modals

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,41 @@
 
 var video = document.getElementById("bannerVid");
-// var modal = document.getElementById("modal");
-// var img = document.getElementById("myImg");
-// var modalContent = document.getElementById("desc");
-// var span = document.getElementsByClassName("close")[0];
+var modal = document.getElementById("portModal");
+var span = document.getElementsByClassName("modalClose")[0];
+var slideIndex = 1;
+showSlides(slideIndex);
 
 video.playbackRate = 0.75;
 
-// img.onclick = function() {
-// modal.style.display = "block";
-// modalContent.innerHTML = this.innerHTML;
-// }
+function openModal() {
+modal.style.display = "block";
 
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace("active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += "active";
+    }
+
 
 function openSideNav() {
     document.getElementById("slideNav").style.height = "100%";
@@ -24,32 +45,7 @@ function closeNav() {
     document.getElementById("slideNav").style.height = "0%";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// next or previous
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
-// thumbsnail control
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    var dots = document.getElementsByClassName("opacity");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
 
